a4fda0e5a783e227f17456a6359e2079227dca01
Prajwal <khotprajwal04@gmail.com>
2025-02-20 17:33:42 -0600

    Header setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="public\globe.png" alt="Logo"/>
+            <span>my travel journal</span>
+        </header>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import App from './App'
+
+/**
+ * Challenge:
+ * 
+ * - Create an App component in a separate file.
+ *   Import it here and render it
+ * - Create a "components" folder and a Header component.
+ *   Render the Header inside the App component.
+ * - Follow the Travel Journal design to build the Header
+ *   for our page.
+ */
 
 
 createRoot(document.getElementById('root')).render(
   <>
-    <p>Empty</p>
+    <App />
   </>
 )
